4c95b34662a52b0d0aac37c6920cecb27d6d4dc1
shanmugam1309 <shanmugaprianvelan2001@gmail.com>
2023-11-16 14:44:57 +0530

    Add project files.

---
+++ Oops Concept/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concept
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            order order = new order();
+            order.Orderno = "123";
+        }
+    }
+}
---
+++ Oops Concept/order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concept
+{
+    public class order
+    {
+        public string Orderno { get; set; }
+        public string Ordervalue { get; set; }
+        public string Ordercountry { get; set; }
+
+        public string createorder()  
+        {
+            return "Order Created";
+        }
+
+        public bool ValidateOrdervalue()
+        {
+            return true;
+        }
+
+        public bool ValidateOrdercountry()
+        {
+            return true;
+        }
+
+    }
+}
+
